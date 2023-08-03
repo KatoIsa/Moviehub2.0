ac80ace78f426d48e548138af16df5be4f9d7ead
Taleem <taleemmankuer@gmail.com>
2023-08-03 13:13:54 +0300

    initial commit

--- src/hooks/useGenre.js
+++
@@ -1,8 +0,0 @@
-const useGenres = (selectedgenre) => {
-  if (selectedgenre < 1) return "";
-
-  const GenresId = selectedgenre.map((g) => g.id);
-  return GenresId.reduce((acc, curr) => acc + "," + curr);
-};
-
-export default useGenres
