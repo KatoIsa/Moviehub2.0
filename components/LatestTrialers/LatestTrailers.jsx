a4ff9b97280e2f192adcdc05e2b1aabd395d34ba
Taleem <taleemmankuer@gmail.com>
2023-08-07 14:11:36 +0300

    add latest trailers section

---
+++ components/LatestTrialers/LatestTrailers.jsx
@@ -0,0 +1,65 @@
+"use client";
+import { useState, useEffect } from "react";
+import styles from "./LatestTrailers.module.scss";
+import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
+import axios from "axios";
+import Iframe from "../Iframe/Iframe";
+
+const LatestTrailers = ({ open, handleopen, movieid, setmovieid }) => {
+  const [movies, setmovies] = useState([]);
+  const [movies2, setmovies2] = useState([]);
+  const fetchUpcomming = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/upcoming?language=en-US&api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY}&page=1`
+    );
+    setmovies(data.results);
+    movies.map((movie) => {
+      setmovies2((curr) => [
+        ...curr,
+        {
+          name: movie.title,
+          id: movie.id,
+          backdropPath: movie.backdrop_path,
+        },
+      ]);
+    });
+  };
+  useEffect(() => {
+    fetchUpcomming();
+  }, []);
+  return (
+    <section className={styles.container}>
+      <div className={styles.column}>
+        <h1>Latest Trailers</h1>
+        <div className={styles.videos}>
+          {movies.map((movie, _i) => (
+            <div
+              onClick={() => {
+                handleopen(movie);
+              }}
+              key={movie.id}
+              className={styles.video}
+            >
+              <div className={styles.image}>
+                <Image
+                  src={`https://www.themoviedb.org/t/p/w355_and_h200_multi_faces${movie.backdrop_path}`}
+                  width={1000}
+                  height={1000}
+                  alt="img"
+                />
+                <FaPlay />
+              </div>
+              <div className={styles.info}>
+                <h2>{movie.title}</h2>
+                <h3>Official trailer</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LatestTrailers;
