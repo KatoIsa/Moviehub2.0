3934b965a4a8801f4b0d4b0bb50c48d891d52e0b
Taleem <taleemmankuer@gmail.com>
2023-08-07 17:54:03 +0300

    add right arrow on h2 headings

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -1,18 +1,23 @@
 import styles from "./Home.module.scss";
 import Card from "../Card/Card";
+import { BsArrowRight } from "react-icons/bs";
 
 const Homepage = ({ title, movies, tv }) => {
   return (
     <section className={styles.container}>
       <div className={styles.div}>
         <h1>{title}</h1>
-        <h2>Movies</h2>
+        <h2>
+          Movies <BsArrowRight />
+        </h2>
         <div className={styles.movies}>
           {movies.map((item, i) => (
             <Card item={item} key={i} type={"movie"} />
           ))}
         </div>
-        <h2>Tv Shows</h2>
+        <h2>
+          Tv Shows <BsArrowRight />
+        </h2>
         <div className={styles.tv}>
           {tv.map((item, i) => (
             <Card item={item} type={"tv"} key={i} />
