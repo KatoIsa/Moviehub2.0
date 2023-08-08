fa1c21bd894d315dc979db56c57c34ed299f7995
Taleem <taleemmankuer@gmail.com>
2023-08-08 15:07:48 +0300

    update video fetch link

--- components/Iframe/Iframe.jsx
+++ components/Iframe/Iframe.jsx
@@ -4,11 +4,11 @@ import styles from "./Iframe.module.scss";
 import { AiOutlineClose } from "react-icons/ai";
 import axios from "axios";
 
-const Iframe = ({ open, movieid, handleclose }) => {
+const Iframe = ({ open, movieid, handleclose, type }) => {
   const [trailers, setTrailers] = useState([]);
   const fetchTrailer = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/movie/${movieid}/videos?api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY}&page=1`
+      `https://api.themoviedb.org/3/${type}/${movieid}/videos?api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY}&page=1`
     );
     setTrailers(data.results);
     console.log(trailers);
