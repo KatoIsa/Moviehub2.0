58d8030ab9b867746358a91863ecb0e62e3c52ac
Taleem <taleemmankuer@gmail.com>
2023-08-15 18:28:15 +0300

    update

--- app/movies/page.jsx
+++ app/movies/page.jsx
@@ -1,54 +1,7 @@
 import React from "react";
 
 const Page = () => {
-  return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ullam
-      explicabo consectetur repellat pariatur necessitatibus odit deleniti ea
-      eligendi maxime a ducimus nam error incidunt repellendus accusamus
-      aspernatur? Maxime nemo quisquam provident vel tempore, exercitationem eos
-      neque, voluptates nisi deserunt reprehenderit placeat eligendi suscipit
-      recusandae porro nam. Eius porro asperiores ipsum corrupti temporibus
-      incidunt, numquam iusto adipisci, facere officia commodi illum vel a culpa
-      quas sit sapiente quod sed? Mollitia saepe ut veritatis eius aliquid cum
-      laboriosam consequuntur veniam deserunt tenetur dolores quae esse
-      asperiores assumenda repudiandae non molestias modi vel similique
-      accusamus ea, quasi magnam commodi iste? Unde nam reiciendis vel quae
-      veniam aperiam saepe, dignissimos dolorem culpa. Labore iure aliquid est
-      quibusdam deserunt quaerat corrupti ullam neque illum, ut, quis
-      voluptatibus quasi nemo consectetur officia harum beatae ad earum quo
-      voluptatem nam necessitatibus excepturi qui? Harum atque, culpa iure nobis
-      asperiores quo voluptates pariatur voluptatibus, nisi cum fugit sunt quae
-      id magni. Quae, atque? Consectetur ratione repellendus quia voluptatibus
-      numquam quo rerum. Illum tenetur enim, laboriosam adipisci quidem eos
-      consequatur sed, ratione amet necessitatibus omnis voluptates temporibus
-      assumenda, ipsam dolor placeat reiciendis accusantium nam corrupti natus
-      eligendi voluptas asperiores debitis iure. Natus, expedita. Sapiente
-      voluptate itaque sit enim ipsam eum ab a voluptas sunt optio modi,
-      voluptatum iusto! Nobis voluptate ipsa culpa quasi perferendis enim,
-      impedit ipsum harum assumenda quis vitae, quod distinctio dolore, quae
-      alias excepturi? Quo consequuntur dolorem, eaque veritatis architecto
-      culpa repudiandae earum id molestias minus, exercitationem aperiam ea
-      fugit consequatur doloribus neque. Culpa voluptates rem modi quaerat error
-      doloribus enim dignissimos minima consequuntur? Reprehenderit aliquid
-      ipsum ut odio, tempore sequi distinctio molestias magni repellendus atque,
-      praesentium labore consectetur ea sapiente est magnam dolores molestiae
-      eum itaque! Aliquam, consequatur repellendus mollitia assumenda ducimus
-      tenetur explicabo facere sapiente sunt repudiandae architecto cum impedit.
-      Nam dolorem doloremque dignissimos exercitationem a sequi vero repudiandae
-      deleniti sit impedit? Suscipit velit et delectus ea non quis saepe illum
-      sunt numquam at nulla perspiciatis ad animi nihil illo necessitatibus ipsa
-      ipsam voluptatem vitae ipsum aut, esse dolore possimus laudantium.
-      Accusantium hic illo eligendi ipsum velit fugit praesentium unde aperiam
-      quisquam. Quam, odio corporis, maxime ea voluptate magnam provident nam id
-      explicabo sunt repudiandae ut aperiam fuga quaerat veniam, ipsam qui rem
-      sed. Consequuntur nihil quod nobis numquam corporis magnam exercitationem
-      esse praesentium asperiores, culpa animi dolore tempore ad deleniti
-      laboriosam voluptas, eligendi vero sapiente assumenda eos. Nulla optio
-      debitis saepe culpa?
-      <h1>Movies page Under development</h1>
-    </div>
-  );
+  return <div>Movies page</div>;
 };
 
 export default Page;
