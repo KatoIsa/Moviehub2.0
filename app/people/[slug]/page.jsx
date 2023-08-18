e2e338cc0ae11a72f553a904d1e1e8a6e69ad7b5
Taleem <taleemmankuer@gmail.com>
2023-08-18 09:39:15 +0300

    add people page

---
+++ app/people/[slug]/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Person.module.scss";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>the people</h1>
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/people/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./People.module.scss";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>The people</h1>
+    </div>
+  );
+};
+
+export default Page;
