36f759ea487303b4a02d74b450849650a000490a
Taleem <taleemmankuer@gmail.com>
2023-08-19 11:25:25 +0300

    add subnav

--- components/subnav/Subnav.jsx
+++ components/subnav/Subnav.jsx
@@ -2,22 +2,7 @@ import styles from "./Subnav.module.scss";
 import Link from "next/link";
 
 const Subnav = ({ mediatype, id }) => {
-  return (
-    <div className={styles.heading}>
-      <Link href={`/${mediatype}/${id}`}>
-        <h2>Overview</h2>
-      </Link>
-      <Link href={`/${mediatype}/${id}/media`}>
-        <h2>Media</h2>
-      </Link>
-      <Link href={`/${mediatype}/${id}/reviews`}>
-        <h2>Reviews</h2>
-      </Link>
-      <Link href={`/${mediatype}/${id}/cast`}>
-        <h2>Full Cast and Crew</h2>
-      </Link>
-    </div>
-  );
+  return c;
 };
 
 export default Subnav;
