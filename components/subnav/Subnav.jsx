07686bde2e878bf9bc0ce7dd3ef72d4c34e2d119
Taleem <taleemmankuer@gmail.com>
2023-08-19 11:28:21 +0300

    reset

--- components/subnav/Subnav.jsx
+++ components/subnav/Subnav.jsx
@@ -2,7 +2,22 @@ import styles from "./Subnav.module.scss";
 import Link from "next/link";
 
 const Subnav = ({ mediatype, id }) => {
-  return c;
+  return (
+    <div className={styles.heading}>
+      <Link href={`/${mediatype}/${id}`}>
+        <h2>Overview</h2>
+      </Link>
+      <Link href={`/${mediatype}/${id}/media`}>
+        <h2>Media</h2>
+      </Link>
+      <Link href={`/${mediatype}/${id}/reviews`}>
+        <h2>Reviews</h2>
+      </Link>
+      <Link href={`/${mediatype}/${id}/cast`}>
+        <h2>Full Cast and Crew</h2>
+      </Link>
+    </div>
+  );
 };
 
 export default Subnav;
