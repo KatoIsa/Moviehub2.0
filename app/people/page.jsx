0cb7040b5114b3e4668206d289030766997d83ff
Taleem <taleemmankuer@gmail.com>
2023-08-20 14:41:05 +0300

    add page pagination

--- app/people/page.jsx
+++ app/people/page.jsx
@@ -3,19 +3,23 @@ import { useState, useEffect } from "react";
 import styles from "./People.module.scss";
 import Person from "@/components/Person/Person";
 import axios from "axios";
+import Custompagination from "@/components/CustomPagination/CustomPagination";
 
 const Page = () => {
   const [thepeople, setthepeople] = useState([]);
+  const [page, setPage] = useState();
+  const [numberofpages, setNumberofpages] = useState();
   const fetchdata = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/person/popular?api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/person/popular?api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY}&language=en-US&page=${page}`
     );
     setthepeople(data.results);
     console.log(data);
+    setNumberofpages(data.total_pages);
   };
   useEffect(() => {
     fetchdata();
-  }, []);
+  }, [page]);
   return (
     <div className={styles.container}>
       <h2>Popular people</h2>
@@ -24,6 +28,7 @@ const Page = () => {
           <Person key={p.id} person={p} />
         ))}
       </div>
+      <Custompagination setPage={setPage} numberofpages={numberofpages} />
     </div>
   );
 };
