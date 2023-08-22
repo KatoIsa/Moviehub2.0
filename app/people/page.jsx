99b88b9c33abb568ac129d89a01bae7f987c40d6
Taleem <taleemmankuer@gmail.com>
2023-08-22 16:36:43 +0300

    add loading screen

--- app/people/page.jsx
+++ app/people/page.jsx
@@ -9,17 +9,21 @@ const Page = () => {
   const [thepeople, setthepeople] = useState([]);
   const [page, setPage] = useState();
   const [numberofpages, setNumberofpages] = useState();
+  const [loading, setloading] = useState(true);
   const fetchdata = async () => {
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/person/popular?api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY}&language=en-US&page=${page}`
     );
     setthepeople(data.results);
     setNumberofpages(data.total_pages);
+    setloading(false);
   };
   useEffect(() => {
     fetchdata();
   }, [page]);
-  return (
+  return loading ? (
+    <div className="loading">Loading</div>
+  ) : (
     <div className={styles.container}>
       <h2>Popular people</h2>
       <div className={styles.peoplecontainer}>
