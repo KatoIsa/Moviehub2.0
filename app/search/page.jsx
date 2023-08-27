11f73e88da2bf02c39fdb4f0538516c099812181
Taleem <taleemmankuer@gmail.com>
2023-08-27 17:02:27 +0300

    add search page

---
+++ app/search/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Search.module.scss";
+
+const Page = () => {
+  return (
+    <div className={styles.container}>
+      <h1>search</h1>
+    </div>
+  );
+};
+
+export default Page;
